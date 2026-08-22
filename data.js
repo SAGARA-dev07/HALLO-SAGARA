8413940ccd301254c5f9d37bfc4d51105e4ff164
SAGARA-dev07 <restuadi0707@gmail.com>
2026-08-23 02:03:27 +0700

    ess

--- data.js
+++ data.js
@@ -1,3 +1,31 @@
 const namaSaya = "sagara";
-let usia = 29;
-console.log(`nama saya adalah ${nama} dan usia saya ${usia} tahun`)
+let usia = 30;
+
+let biodata = document.getElementById('biodata');
+
+function generateBiodata() {
+    let generasi;
+
+   if (usia > 15 && usia < 17) {
+    generasi = "generasi remaja";
+   } 
+   else if (usia > 18 && < 30) {
+    generasi = "generasi dewasa";
+   } 
+   else if (usia >= 30) {
+    generasi = "generasi tua";
+   }
+   else if (usia > 2 && usia < 10) {
+    generasi = "generasi anak anak";
+   }
+   else {
+    generasi = "generasi balita";
+   }
+
+   return biodata.innerHTML = generasi;
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
